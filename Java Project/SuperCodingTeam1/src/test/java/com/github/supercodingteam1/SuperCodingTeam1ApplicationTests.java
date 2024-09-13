b67a113c67d8bc1ab494b5d891305b54db6f3084
TaeYoung1217 <wkdxodud44@gmail.com>
2024-09-13 13:02:12 +0900

    initial commit

---
+++ Java Project/SuperCodingTeam1/src/test/java/com/github/supercodingteam1/SuperCodingTeam1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.supercodingteam1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SuperCodingTeam1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
