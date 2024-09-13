357be530230f1fa27bf38799a4c5168f39997f8f
TaeYoung1217 <wkdxodud44@gmail.com>
2024-09-13 13:25:49 +0900

    initial commit

---
+++ src/main/java/com/github/supercodingteam1/SuperCodingTeam1Application.java
@@ -0,0 +1,13 @@
+package com.github.supercodingteam1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SuperCodingTeam1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SuperCodingTeam1Application.class, args);
+    }
+
+}
