f74879f8c39aa4886be6025c51b8965a33be82db
A-RAJO <149932579+A-RAJO@users.noreply.github.com>
2024-09-14 00:03:57 +0900

    feat: user repository setting

---
+++ src/main/java/com/github/supercodingteam1/repository/user/UserRepository.java
@@ -0,0 +1,9 @@
+package com.github.supercodingteam1.repository.user;
+
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User,Integer> {
+}
