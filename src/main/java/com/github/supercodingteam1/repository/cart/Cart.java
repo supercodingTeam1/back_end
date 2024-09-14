6798822300b8018dad4b172d8f627682f56b10de
A-RAJO <149932579+A-RAJO@users.noreply.github.com>
2024-09-14 21:50:32 +0900

    fix: cart entity 연관관계, 이름수정

--- src/main/java/com/github/supercodingteam1/repository/cart/Cart.java
+++ src/main/java/com/github/supercodingteam1/repository/cart/Cart.java
@@ -1,33 +1,26 @@
 package com.github.supercodingteam1.repository.cart;
 
-import com.github.supercodingteam1.repository.option.Option;
 import com.github.supercodingteam1.repository.user.User;
 import jakarta.persistence.*;
 import lombok.*;
-
-import java.util.List;
 
 @Getter
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
-@Table(name = "carts")
+@Table(name = "cart")
 @Entity
 public class Cart {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "cart_id", nullable = false)
+    @Column(name = "cart_id", nullable = false, length = 11)
     private Integer cartId;
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "user_id",nullable = false)
     private User user;
 
-    @ManyToMany(fetch = FetchType.LAZY)
-    @JoinColumn(name = "option_id", nullable = false)
-    private List<Option> optionList;
-
-    @Column(name = "quantity", nullable = false)
-    private Integer quantity;
+    @Column(name = "cart_quantity", nullable = false, length = 11)
+    private Integer cartQuantity;
 }
