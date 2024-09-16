cba04bc3873f20740a1b7c9737e90c3704894f4f
TaeYoung1217 <wkdxodud44@gmail.com>
2024-09-16 17:46:30 +0900

    feat : getAllItems 메소드 중 sort=sales&order 구현 완료

--- src/main/java/com/github/supercodingteam1/service/ItemService.java
+++ src/main/java/com/github/supercodingteam1/service/ItemService.java
@@ -1,5 +1,6 @@
 package com.github.supercodingteam1.service;
 
+import com.github.supercodingteam1.repository.item.Item;
 import com.github.supercodingteam1.repository.item.ItemRepository;
 import com.github.supercodingteam1.repository.option.OptionRepository;
 import com.github.supercodingteam1.service.mapper.OptionToGetAllItemDTOMapper;
@@ -10,6 +11,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
 import java.util.Arrays;
+import java.util.Comparator;
 import java.util.List;
 
 @Service
@@ -32,9 +34,35 @@ public class ItemService {
                                 .build();
                     }).toList();
         }else { //request param으로 sort와 order가 들어올때
-            return Arrays.asList(new GetAllItemDTO[]{});
+            if(order.equals("asc")) {
+                return itemRepository.findAll().stream()
+                        .sorted(Comparator.comparing(Item::getTotalSales))
+                        .map(item -> {
+                            return GetAllItemDTO.builder()
+                                    .item_id(item.getItemId())
+                                    .item_name(item.getItemName())
+                                    .item_image(item.getImageList().get(0).getImageLink())
+                                    .category(item.getCategory())
+                                    .option(OptionToGetAllItemDTOMapper.INSTANCE.OptionToGetAllItemOptionDTO(optionRepository.findAllByItem(item)))
+                                    .price(item.getItemPrice())
+                                    .build();
+                        }).toList();
+            }
+            else{
+                return itemRepository.findAll().stream()
+                        .sorted(Comparator.comparing(Item::getTotalSales).reversed())
+                        .map(item -> {
+                            return GetAllItemDTO.builder()
+                                    .item_id(item.getItemId())
+                                    .item_name(item.getItemName())
+                                    .item_image(item.getImageList().get(0).getImageLink())
+                                    .category(item.getCategory())
+                                    .option(OptionToGetAllItemDTOMapper.INSTANCE.OptionToGetAllItemOptionDTO(optionRepository.findAllByItem(item)))
+                                    .price(item.getItemPrice())
+                                    .build();
+                        }).toList();
+            }
         }
-
     }
 
     public List<GetAllItemDTO> getAllItemsOrderBySales(String sort, String order) {
