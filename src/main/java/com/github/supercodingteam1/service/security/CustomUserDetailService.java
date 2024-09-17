fa18efdc1653e92d0c2a01c26c77c6d720655044
A-RAJO <149932579+A-RAJO@users.noreply.github.com>
2024-09-17 15:52:54 +0900

    feat: UserDetails, UserDetailService setting

---
+++ src/main/java/com/github/supercodingteam1/service/security/CustomUserDetailService.java
@@ -0,0 +1,35 @@
+package com.github.supercodingteam1.service.security;
+
+import com.github.supercodingteam1.repository.UserDetails.CustomUserDetails;
+import com.github.supercodingteam1.repository.user.User;
+import com.github.supercodingteam1.repository.user.UserRepository;
+import com.github.supercodingteam1.web.exceptions.NotFoundException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Primary;
+import org.springframework.data.crossstore.ChangeSetPersister;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Primary
+@RequiredArgsConstructor
+@Service
+public class CustomUserDetailService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        User user=userRepository.findByEmail(email).orElseThrow(()->new NotFoundException("email에 해당하는 user가 없습니다."));
+
+        CustomUserDetails customUserDetails=CustomUserDetails.builder()
+                .userId(user.getUserId())
+                .email(user.getEmail())
+                .password(user.getPassword())
+                .authorities(user.getRoles())
+                .build();
+
+        return customUserDetails;
+
+    }
+}
