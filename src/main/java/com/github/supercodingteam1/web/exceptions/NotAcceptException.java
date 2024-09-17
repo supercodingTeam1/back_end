ec99bdfcfc40c142c31306e119996f2ed24fa58b
A-RAJO <149932579+A-RAJO@users.noreply.github.com>
2024-09-17 19:55:57 +0900

    feat: NotFound, NotAccept exception setting

--- src/main/java/com/github/supercodingteam1/web/exceptions/NotAcceptException.java
+++ src/main/java/com/github/supercodingteam1/web/exceptions/NotAcceptException.java
@@ -3,9 +3,9 @@ package com.github.supercodingteam1.web.exceptions;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-@ResponseStatus(HttpStatus.NOT_ACCEPTABLE)
-public class NotFoundException extends RuntimeException {
-    public NotFoundException(String message) {
+@ResponseStatus(HttpStatus.NOT_FOUND)
+public class NotAcceptException extends RuntimeException {
+    public NotAcceptException(String message) {
         super(message);
     }
 }
