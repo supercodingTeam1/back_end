17465dacfea63e875eca7db2eff2e3caa374c1d6
TaeYoung1217 <wkdxodud44@gmail.com>
2024-09-18 14:47:52 +0900

    feat : 장바구니에 물품 추가 기능 구현 중

--- src/main/java/com/github/supercodingteam1/web/controller/CartController.java
+++ src/main/java/com/github/supercodingteam1/web/controller/CartController.java
@@ -1,8 +1,11 @@
 package com.github.supercodingteam1.web.controller;
 
+import com.github.supercodingteam1.service.CartService;
 import com.github.supercodingteam1.service.ItemService;
 import com.github.supercodingteam1.web.dto.AddToCartDTO;
 import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -13,11 +16,15 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/cart")
 @RequiredArgsConstructor
 public class CartController {
+    private static final Logger log = LoggerFactory.getLogger(CartController.class);
     private final ItemService itemService;
+    private final CartService cartService;
 
     @PostMapping
-    public ResponseEntity<?> addItemToCart(@RequestBody AddToCartDTO addToCartDTO) {
+    public ResponseEntity<?> addItemToCart(@RequestBody AddToCartDTO addToCartDTO) { //장바구니 담기
+        log.info("addItemCart 메소드 호출, {},{}", addToCartDTO.getOption_id(), addToCartDTO.getQuantity());
 
+        cartService.addItemToCart(addToCartDTO);
         return ResponseEntity.ok().build();
     }
 }
