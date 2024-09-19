00dd7d1ae67a65ab8cca059945427f67936d7bb1
TaeYoung1217 <wkdxodud44@gmail.com>
2024-09-19 15:52:19 +0900

    feat : AWS S3 사진 업로드 및 url 다운받아 images 테이블에 저장

--- src/main/java/com/github/supercodingteam1/config/security/SecurityConfiguration.java
+++ src/main/java/com/github/supercodingteam1/config/security/SecurityConfiguration.java
@@ -24,10 +24,11 @@ public class SecurityConfiguration {
                 .sessionManagement(session -> session
                         .sessionCreationPolicy(SessionCreationPolicy.STATELESS))  // Stateless 설정
                 .authorizeHttpRequests(authorize -> authorize
-                        .anyRequest().authenticated())  // 모든 요청 인증 필요
+                        .anyRequest().permitAll())  // 모든 요청 인증 필요
                 .formLogin(form -> form.disable())  // 폼 로그인 비활성화
                 .httpBasic(httpBasic -> httpBasic.disable())  // HTTP Basic 인증 비활성화
                 .rememberMe(rememberMe -> rememberMe.disable());  // Remember Me 기능 비활성화
+
 
         // 기존 frameOptions().sameOrigin() 설정에 해당하는 부분을 직접 설정
         http.headers(headers -> headers.frameOptions(frameOptions -> frameOptions.sameOrigin()));
---
+++ src/main/java/com/github/supercodingteam1/service/S3Uploader.java
@@ -0,0 +1,78 @@
+package com.github.supercodingteam1.service;
+
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.model.CannedAccessControlList;
+import com.amazonaws.services.s3.model.PutObjectRequest;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+@RequiredArgsConstructor
+@Service
+@Slf4j
+public class S3Uploader {
+
+    private final AmazonS3 amazonS3;
+
+    @Value("${cloud.aws.s3.bucket}")
+    private String bucket;
+
+    public String upload(MultipartFile multipartFile, String dirName) throws IOException {
+        File uploadFile = convert(multipartFile)
+                .orElseThrow(() -> new IllegalArgumentException("MultipartFile -> File 전환 실패"));
+        return upload(uploadFile, dirName);
+    }
+    private String upload(File uploadFile, String dirName) {
+        String fileName = dirName + "/" + uploadFile.getName();
+        String uploadImageUrl = putS3(uploadFile, fileName);
+
+        removeNewFile(uploadFile);  // convert()함수로 인해서 로컬에 생성된 File 삭제 (MultipartFile -> File 전환 하며 로컬에 파일 생성됨)
+
+        return uploadImageUrl;      // 업로드된 파일의 S3 URL 주소 반환
+    }
+
+    private String putS3(File uploadFile, String fileName) {
+        amazonS3.putObject(
+                new PutObjectRequest(bucket, fileName, uploadFile)
+                        .withCannedAcl(CannedAccessControlList.PublicRead)	// PublicRead 권한으로 업로드 됨
+        );
+        return amazonS3.getUrl(bucket, fileName).toString();
+    }
+
+    private void removeNewFile(File targetFile) {
+        if(targetFile.delete()) {
+            log.info("파일이 삭제되었습니다.");
+        }else {
+            log.info("파일이 삭제되지 못했습니다.");
+        }
+    }
+
+    private Optional<File> convert(MultipartFile file) throws  IOException {
+        File convertFile = new File(file.getOriginalFilename()); // 업로드한 파일의 이름
+        if(convertFile.createNewFile()) {
+            try (FileOutputStream fos = new FileOutputStream(convertFile)) {
+                fos.write(file.getBytes());
+            }
+            return Optional.of(convertFile);
+        }
+        return Optional.empty();
+    }
+
+    public List<String> getUrlFromS3(List<String> filenameList, String dirName){
+        List<String> urlList = new ArrayList<>();
+        for(String filename : filenameList){
+            urlList.add(amazonS3.getUrl(bucket,filename).toString());
+        }
+
+        return urlList;
+    }
+}
