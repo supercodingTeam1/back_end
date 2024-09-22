6b586a1060933cd6a1748020ec1b56ac011afc3a
TaeYoung1217 <wkdxodud44@gmail.com>
2024-09-22 19:08:11 +0900

    feat : order 기능 구현 중

--- src/main/java/com/github/supercodingteam1/repository/entity/order/Order.java
+++ src/main/java/com/github/supercodingteam1/repository/entity/order/Order.java
@@ -1,10 +1,13 @@
 package com.github.supercodingteam1.repository.entity.order;
 
 import com.github.supercodingteam1.repository.entity.cart.Cart;
+import com.github.supercodingteam1.repository.entity.option.Option;
+import com.github.supercodingteam1.repository.entity.user.User;
 import jakarta.persistence.*;
 import lombok.*;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 @Getter
 @Setter
@@ -19,16 +22,29 @@ public class Order {
     @Column(name = "order_id", nullable = false, length = 11)
     private Integer order_id;
 
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private User user;
+
     @Column(name="order_num",nullable = false)
     private String orderNum;
 
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "cart_id", nullable = false)
-    private Cart cart;
+    @OneToMany
+    @JoinColumn(name = "option_id", nullable = false)
+    private List<Option> options;
 
     @Column(name = "order_at", nullable = false)
     private LocalDateTime orderAt;
 
     @Column(name = "order_address",nullable = false, length = 200)
     private String orderAddress;
+
+    @Column(name = "name", nullable = false, length = 20)
+    private String name;
+
+    @Column(name = "payment", nullable = false, length = 20)
+    private String payment;
+
+    @Column(name = "phone_num", nullable = false, length = 15)
+    private String phoneNum;
 }
