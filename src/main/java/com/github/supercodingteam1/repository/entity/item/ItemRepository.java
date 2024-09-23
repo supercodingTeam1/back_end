eaf0eb59cb4f96e3f7e3f1da77d2c8f3fb264be9
A-RAJO <149932579+A-RAJO@users.noreply.github.com>
2024-09-23 23:07:18 +0900

    fit: jpa 이름 변경(findAllByUserId->findAllByUser_UserId), @Param 어노테이션으로 엔티티 명시

--- src/main/java/com/github/supercodingteam1/repository/entity/item/ItemRepository.java
+++ src/main/java/com/github/supercodingteam1/repository/entity/item/ItemRepository.java
@@ -2,11 +2,12 @@ package com.github.supercodingteam1.repository.entity.item;
 
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
 public interface ItemRepository extends JpaRepository<Item,Integer> {
-    List<Item> findAllByUserId(int userId);
+    List<Item> findAllByUser_UserId(@Param("userId") Integer userId);
 }
