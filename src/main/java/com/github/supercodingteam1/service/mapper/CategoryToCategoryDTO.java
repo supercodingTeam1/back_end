7bbd4857a570bae6bff34e4ae682b96a67c5d531
A-RAJO <149932579+A-RAJO@users.noreply.github.com>
2024-09-23 19:38:52 +0900

    feat: category entity to dto mapper setting

---
+++ src/main/java/com/github/supercodingteam1/service/mapper/CategoryToCategoryDTO.java
@@ -0,0 +1,13 @@
+package com.github.supercodingteam1.service.mapper;
+
+import com.github.supercodingteam1.repository.entity.category.Category;
+import com.github.supercodingteam1.web.dto.CategoryDTO;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+
+@Mapper
+public interface CategoryToCategoryDTO {
+    @Mapping(source = "categoryGender", target = "category_gender")
+    @Mapping(source = "categoryType", target = "category_type")
+    CategoryDTO categoryToCategoryDTO(Category category);
+}
