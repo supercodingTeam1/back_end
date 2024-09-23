bf302158fd6f00509022ba3fe7f4d58699abcfab
A-RAJO <149932579+A-RAJO@users.noreply.github.com>
2024-09-23 19:49:41 +0900

    feat: category mapper setting

--- src/main/java/com/github/supercodingteam1/service/mapper/CategoryToCategoryDTO.java
+++ src/main/java/com/github/supercodingteam1/service/mapper/CategoryToCategoryDTO.java
@@ -4,9 +4,11 @@ import com.github.supercodingteam1.repository.entity.category.Category;
 import com.github.supercodingteam1.web.dto.CategoryDTO;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
+import org.mapstruct.factory.Mappers;
 
 @Mapper
 public interface CategoryToCategoryDTO {
+    CategoryToCategoryDTO INSTANCE = Mappers.getMapper(CategoryToCategoryDTO.class);
     @Mapping(source = "categoryGender", target = "category_gender")
     @Mapping(source = "categoryType", target = "category_type")
     CategoryDTO categoryToCategoryDTO(Category category);
