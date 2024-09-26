5c30895b5e6ef716b8e600aef6ab43313216d7c4
A-RAJO <149932579+A-RAJO@users.noreply.github.com>
2024-09-26 19:29:21 +0900

    feat: MyBuyInfoDTO 에 들어가는 자세한 주문내역

---
+++ src/main/java/com/github/supercodingteam1/web/dto/MyBuyDetailInfoDTO.java
@@ -0,0 +1,22 @@
+package com.github.supercodingteam1.web.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.List;
+
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class MyBuyDetailInfoDTO {
+    private Integer order_id;
+    private String item_image;
+    private String item_name;
+    private Double price;
+    private Integer quantity;
+    private String category;
+    private List<OptionDTO> option;  // 옵션 리스트
+}
