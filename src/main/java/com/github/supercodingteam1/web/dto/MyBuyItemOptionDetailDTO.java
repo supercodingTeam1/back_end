254048add5ca8472d8543b05a277d5a41bc8a9c7
A-RAJO <149932579+A-RAJO@users.noreply.github.com>
2024-09-28 14:52:21 +0900

    feat: add @Builder

--- src/main/java/com/github/supercodingteam1/web/dto/MyBuyItemOptionDetailDTO.java
+++ src/main/java/com/github/supercodingteam1/web/dto/MyBuyItemOptionDetailDTO.java
@@ -1,12 +1,10 @@
 package com.github.supercodingteam1.web.dto;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
 @Setter
 @Getter
+@Builder
 @NoArgsConstructor
 @AllArgsConstructor
 public class MyBuyItemOptionDetailDTO {
