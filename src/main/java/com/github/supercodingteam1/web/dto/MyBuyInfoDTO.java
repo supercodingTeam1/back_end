1eaed94131a96b20baba35d71ec457619f8eb398
A-RAJO <149932579+A-RAJO@users.noreply.github.com>
2024-09-28 14:39:24 +0900

    fix: order_num Integer to String

--- src/main/java/com/github/supercodingteam1/web/dto/MyBuyInfoDTO.java
+++ src/main/java/com/github/supercodingteam1/web/dto/MyBuyInfoDTO.java
@@ -1,19 +1,17 @@
 package com.github.supercodingteam1.web.dto;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
 import java.time.LocalDateTime;
 import java.util.List;
 
 @Setter
 @Getter
+@Builder
 @NoArgsConstructor
 @AllArgsConstructor
 public class MyBuyInfoDTO {
-    private Integer order_num;
+    private String order_num;
     private LocalDateTime order_at;
     private List<MyBuyItemDetailDTO> myBuyItemDetailDTOList; // 주문 상품 리스트
 
