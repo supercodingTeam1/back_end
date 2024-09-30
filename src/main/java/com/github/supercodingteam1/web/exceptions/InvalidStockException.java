31fdcef9e4769a3c98ef123db27bbea6d5458b20
A-RAJO <149932579+A-RAJO@users.noreply.github.com>
2024-09-30 19:46:34 +0900

    feat: 재고값을 음수로 입력했을 때의 예외

---
+++ src/main/java/com/github/supercodingteam1/web/exceptions/InvalidStockException.java
@@ -0,0 +1,7 @@
+package com.github.supercodingteam1.web.exceptions;
+
+public class InvalidStockException extends RuntimeException {
+    public InvalidStockException(String message) {
+        super(message);
+    }
+}
