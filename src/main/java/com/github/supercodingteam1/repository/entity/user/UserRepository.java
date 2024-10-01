a44192c881b21671ea209e805e048456d9ff370d
A-RAJO <149932579+A-RAJO@users.noreply.github.com>
2024-10-01 15:07:15 +0900

    feat: add findExistByUserName (비관적 락을 걸고 사용자 이름으로 사용자를 조회하는 메서드)

--- src/main/java/com/github/supercodingteam1/repository/entity/user/UserRepository.java
+++ src/main/java/com/github/supercodingteam1/repository/entity/user/UserRepository.java
@@ -1,7 +1,9 @@
 package com.github.supercodingteam1.repository.entity.user;
 
 
+import jakarta.persistence.LockModeType;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Lock;
 import org.springframework.stereotype.Repository;
 import java.util.Optional;
 import org.springframework.data.repository.query.Param;
@@ -10,8 +12,10 @@ import org.springframework.data.repository.query.Param;
 @Repository
 public interface UserRepository extends JpaRepository<User,Integer> {
     public User findByUserId(Integer userId);
-
-    public User findByUserName(String userName);
+    // 사용자 이름을 통해 비관적 락을 걸고 데이터를 조회하는 메서드
+    @Lock(LockModeType.PESSIMISTIC_WRITE)
+    boolean findExistByUserName(@Param("user_name") String userName);
+    User findByUserName(String userName);
 
     public Optional<User> findByEmail(String email);
 
